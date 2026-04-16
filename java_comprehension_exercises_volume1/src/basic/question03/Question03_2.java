b1c33e983d25ff101aae0d6e0a309099060d5bb8
n-kawahito <n-kawahito@CD26304L.arksystems.co.jp>
2026-04-16 11:08:56 +0900

    question3

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_2.java
@@ -3,12 +3,23 @@ package basic.question03;
 /**
  * 第6章 演算子
  */
+//1. int 型 の変数[basePrice] を初期化して作成する。 
+//➢ 初期値：1200 
+//➢ 通常料金を意味する。 
+//2. int 型の変数[tickets]を初期化して作成する。 
+//➢ 初期値：3 
+//➢ チケットの購入枚数を意味する。 
+//3. int 型の変数[reducedPrice]を初期化して作成する。 
+//➢ 初期値：[basePrice]の値から 200 を引いた値 
+//➢ 最終的な価格を意味する。 
 
 public class Question03_2 {
 
 	public static void main(String[] args) {
 
-		// TODO:ここに実装
-		
+		int basePrice = 1200;
+		int tickets = 3;
+		int reducedPrice = (basePrice - 200);
+
 	}
 }
